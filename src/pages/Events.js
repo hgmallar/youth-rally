3b58a1dc1dbecf33ff642f27ca930b316351ad0c
Hilary Mallar <hilarymallar@gmail.com>
2019-05-15 13:10:23 -0400

    Added donate button to events page

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -18,24 +18,32 @@ export default () => (
             </h2>
             <p>
               Formally Pineapple's Cruise for Crohn's & Colitis, this year's
-              A"WEAR"NESS event will happen again at <a href="https://www.thewestyisabar.com/">The Westy</a> (516 W State St, Ithaca, New York). The Westy's
-              opening the doors to kids for this event (they might even put out
-              the water table!!!) on June 9th from 1PM - 4PM. </p>
-              <p className="font-weight-bold">$20 ticket price at the door includes:</p>
-              <ul>
+              A"WEAR"NESS event will happen again at{" "}
+              <a href="https://www.thewestyisabar.com/">The Westy</a> (516 W
+              State St, Ithaca, New York). The Westy's opening the doors to kids
+              for this event (they might even put out the water table!!!) on
+              June 9th from 1PM - 4PM.{" "}
+            </p>
+            <p className="font-weight-bold">
+              $20 ticket price at the door includes:
+            </p>
+            <ul>
               <li>Live music: Notorious Stringbusters </li>
               <li>Catered snacks: Red's Place </li>
               <li>Draft beer: Bacchus Brewing (+ cash bar OPEN) </li>
               <li>Infamous Silent Auction </li>
-              </ul>
-              <p>Kids 12 & under - $10 </p>
-              <p>Kids 4 & under - FREE </p>
-              <p>Once again, all proceeds will benefit the 2019 Youth Rally - a week
-              long summer camp for youth ages 11-17 living with congenital birth
-              defects, chronic illness, and others who have been victims of
-              physical trauma, many of whom WEAR ostomies or other surgical
-              openings as a result of life-saving/sustaining surgery. </p>
-              <p>What each Youth Rally participant shares in common is some
+            </ul>
+            <p>Kids 12 & under - $10 </p>
+            <p>Kids 4 & under - FREE </p>
+            <p>
+              Once again, all proceeds will benefit the 2019 Youth Rally - a
+              week long summer camp for youth ages 11-17 living with congenital
+              birth defects, chronic illness, and others who have been victims
+              of physical trauma, many of whom WEAR ostomies or other surgical
+              openings as a result of life-saving/sustaining surgery.{" "}
+            </p>
+            <p>
+              What each Youth Rally participant shares in common is some
               disruption/diversion to their bowel and/or bladder system(s) and a
               yearning to feel accepted among their peers. The Youth Rally is
               particularly unique in that the volunteer counselors live with the
@@ -44,11 +52,48 @@ export default () => (
               environment of mentor-ship and self-advocacy where these children
               of varying physical abilities gain independence and self-esteem
               while having fun and without fear of being ostracized for their
-              medical/physical differences. </p>
-              <p>Mark your calendar for this Sunday FUNday and JOIN US! </p>
-            <h2 className="text-center">
-              July 8th - 13th, 2019 Youth Rally in Seattle, WA
-            </h2>
+              medical/physical differences.{" "}
+            </p>
+            <p>Mark your calendar for this Sunday FUNday and JOIN US! </p>
+            <p className="text-center font-weight-bold">
+              Can't make the event, but wish to contribute? Donate here:
+            </p>
+            <form
+              action="https://www.paypal.com/cgi-bin/webscr"
+              method="post"
+              target="_top"
+            >
+              <p className="text-center">
+                <input name="cmd" type="hidden" value="_donations" />
+                <input
+                  name="business"
+                  type="hidden"
+                  value="info@youthrally.org"
+                />
+                <input name="lc" type="hidden" value="US" />
+                <input
+                  name="item_name"
+                  type="hidden"
+                  value="The Youth Rally Committee, Inc."
+                />
+                <input name="no_note" type="hidden" value="0" />
+                <input name="currency_code" type="hidden" value="USD" />
+                <input
+                  name="bn"
+                  type="hidden"
+                  value="PP-DonationsBF:btn_donate_LG.gif:NonHostedGuest"
+                />
+                <input
+                  alt="PayPal - The safer, easier way to pay online!"
+                  name="submit"
+                  src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
+                  type="image"
+                />
+              </p>
+              <h2 className="text-center">
+                July 8th - 13th, 2019 Youth Rally in Seattle, WA
+              </h2>
+            </form>
           </div>
           <hr />
           <div className="row mx-auto">
