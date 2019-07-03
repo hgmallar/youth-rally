afb6e33d08c7cd1716222c7f461d4db524dfbe0a
Hilary Mallar <hilarymallar@gmail.com>
2019-07-03 16:33:44 -0400

    updated photo links

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -11,66 +11,49 @@ export default () => (
 
     <div id="content">
       <div className="col-md-10 mx-auto text-center font-weight-bold">
-        <p className="greenText">
-          <a href="https://youthrally.smugmug.com/">
+        <p className="purpleText">
+          <a href="https://youthrallyphotos.smugmug.com/2018-Rally-in-Boulder">
             2018 Youth Rally Photos and Videos
           </a>
         </p>
+        <p className="greenText">
+          <a href="https://youthrallyphotos.smugmug.com/2017-Youth-Rally">
+            2017 Youth Rally Photos
+          </a>
+        </p>
         <p className="purpleText">
-          <a href="https://youthrally.smugmug.com/Youth-Rally-2017-Summer-of-Love/n-RkThLs/">
-            2017 "<span className="font-italic">Summer of Love</span>" photo booth
-            shots
+          <a href="https://youthrallyphotos.smugmug.com/2017-Rally-in-San-Diego-Video">
+            2017 Youth Rally Videos
           </a>
         </p>
         <p className="greenText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2017-Youth-Rally">
-            2017 Youth Rally Photos and Videos
+          <a href="https://youthrallyphotos.smugmug.com/Youth-Rally-2016-in-Seattle">
+            2016 Youth Rally Photos
           </a>
         </p>
         <p className="purpleText">
-          <a href="https://seattlepartyshots.smugmug.com/Other/07-29-16-Youth-Rally-2016/">
-            2016 "<span className="font-italic">Under the Sea</span>" photo booth
-            shots
+          <a href="https://youthrallyphotos.smugmug.com/2016-Rally-in-Seattle-Videos">
+            2016 Youth Rally Videos
           </a>
         </p>
         <p className="greenText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/Youth-Rally-2016-in-Seattle">
-            2016 Youth Rally Photos and Videos
+          <a href="https://youthrallyphotos.smugmug.com/2015RallyinBoulder">
+            2015 Youth Rally Photos
           </a>
         </p>
         <p className="purpleText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2015RallyinBoulder">
-            2015 Youth Rally Photos and Videos
+          <a href="https://youthrallyphotos.smugmug.com/2015-Rally-in-Boulder-Videos">
+            2015 Youth Rally Videos
           </a>
         </p>
         <p className="greenText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2014RallyinSanDiego">
-            2014 Youth Rally Photos and Videos
+          <a href="https://youthrallyphotos.smugmug.com/2014RallyinSanDiego">
+            2014 Youth Rally Photos
           </a>
         </p>
         <p className="purpleText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/AdditionalMaterials/OtherRallyInformation/25059403_Cnz3sS#2053472067_mDm8qF3/A">
-            2013 Youth Rally Promotional Video
-          </a>
-        </p>
-        <p className="greenText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2013-YouthRallyinSeattle">
-            2013 Youth Rally Photos and Videos
-          </a>
-        </p>
-        <p className="purpleText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2012RallyBoulderColorado">
-            2012 Youth Rally Photos and Videos
-          </a>
-        </p>
-        <p className="greenText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2011RallyinSanDiego">
-            2011 Youth Rally Photos and Videos
-          </a>
-        </p>
-        <p className="purpleText">
-          <a href="https://youthrallyphotos.smugmug.com/YouthRallyPhotos/2010-Rally-in-Cincinnati-Ohio">
-            2010 Youth Rally Photos
+          <a href="https://youthrallyphotos.smugmug.com/2014-Rally-in-San-Diego-Videos">
+            2014 Youth Rally Videos
           </a>
         </p>
       </div>
