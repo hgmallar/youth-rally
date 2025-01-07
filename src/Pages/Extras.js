85539015f938bfe2ae1348882c7c292866d39bb2
Hilary <hilarymallar@gmail.com>
2025-01-06 19:14:50 -0500

    Added extras for 2025

--- src/Pages/Extras.js
+++ src/Pages/Extras.js
@@ -3,7 +3,7 @@ import React from "react";
 const PDFViewer = () => {
  return (
  <div>
- <iframe title="2024 Extra Add Ons" src="/assets/documents/2024AddOns.pdf" width="100%" height="1066px"/>
+ <iframe title="2025 Extra Add Ons" src="/assets/documents/2025AddOns.pdf" width="100%" height="1066px"/>
  </div>
  );
 };
